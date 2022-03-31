7501e199af4554e6a43bbeb8f78f46fcd6b7dbe2
Nazar Gaiduk <child.tmabc@gmail.com>
2022-03-31 19:14:09 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryList = createListOfGallery(galleryItems);
+
+function createListOfGallery(list) {
+    return galleryItems.map(list => `
+    <a class="gallery__item" href="${list.original}">
+  <img class="gallery__image" src="${list.preview}" alt="${list.description}" />
+</a>`).join('');
+}
+
+galleryEl.innerHTML = galleryList;
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, captionPosition: 'bottom'});
+console.log(lightbox);
+
+
+
+
+
+
+
